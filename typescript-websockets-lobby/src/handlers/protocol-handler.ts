28fa2fc21021cf5e1861e525deb6a9c417ebdf53
Sam <samukagamer10000@gmail.com>
2026-03-09 13:43:47 -0300

    Implement game in progress marking and notifications
    Added functionality to mark the game as in progress and notify players.

--- typescript-websockets-lobby/src/handlers/protocol-handler.ts
+++ typescript-websockets-lobby/src/handlers/protocol-handler.ts
@@ -95,6 +95,9 @@ export class ProtocolHelper {
 					LoggerHelper.logInfo('Recieved Lobby Start ' + message);
 					ProtocolHelper.startGameForLobby(gameServer, clientSocket, message, turnKey);
 					break;
+				case EAction.GameConnectionsFinished:
+					ProtocolHelper.markGameInProgress(gameServer, clientSocket);
+					break;
 				case EAction.Offer:
 				case EAction.Answer:
 				case EAction.Candidate:
@@ -458,6 +461,30 @@ export class ProtocolHelper {
 			}
 		} catch (err: any) {
 			LoggerHelper.logError(`[ProtocolHelper.startGameForLobby()] An error had occurred while parsing a message: ${err}`);
+		}
+	};
+
+	// Experimental: Stop player from entering game in progress
+	private static markGameInProgress = (gameServer: GameServerHandler, clientSocket: ClientSocket) => {
+		try {
+			const lobby = gameServer.getLobbyByPlayerId(clientSocket.id);
+			
+			if (lobby) {
+				lobby.inProgress = true;
+				LoggerHelper.logInfo(`Lobby ${lobby.id} marked as in progress`);
+				
+				// Notify all players in the lobby
+				lobby.players.forEach((player) => {
+					ProtocolHelper.sendLobbyChanged(player, lobby);
+				});
+				
+				// Update lobby list for all clients
+				gameServer.connectedClients.forEach((client) => {
+					ProtocolHelper.sendLobbyList(gameServer, client);
+				});
+			}
+		} catch (err: any) {
+			LoggerHelper.logError(`[ProtocolHelper.markGameInProgress()] An error had occurred: ${err}`);
 		}
 	};
 
